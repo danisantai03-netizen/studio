99b3b69b582ebd3137099b594439184728a7b15a
Dani Santai <danisantai03@gmail.com>
2025-08-29 19:08:41 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
-import { AppProviders } from '@/hooks/use-notifications';
+import { AppProviders } from '@/hooks/use-notifications.tsx';
 
 export const metadata: Metadata = {
   title: 'GreenEarth',
