3c021148f7851102ef53bc23e895205e18a2e66e
AnonymousTsm <danisantai03@gmail.com>
2025-09-05 04:36:16 +0700

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,16 +1,12 @@
-
 { pkgs, ... }: {
-  # Versi Nix channel
   channel = "stable-24.11";
 
-  # Tools yang tersedia di environment
   packages = [
     pkgs.nodejs_20
     pkgs.nodePackages.firebase-tools
     pkgs.git
   ];
 
-  # Konfigurasi Preview
   idx.previews = {
     enable = true;
     previews = {
@@ -18,13 +14,12 @@
         manager = "web";
         cwd = "user";
         command = [
-          "npm" "run" "dev" "--" "-p" "$PORT" "-H" "0.0.0.0"
+          "npm" "run" "dev" "--" "--turbopack" "-p" "$PORT" "-H" "0.0.0.0"
         ];
       };
     };
   };
 
-  # Lifecycle hooks: install dependency otomatis
   idx.workspace.onCreate = {
     install-user = "cd user && npm install";
   };
